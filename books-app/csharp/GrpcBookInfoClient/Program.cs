6a170f3ff5fa87c30d181ac12716102e84b903d2
Stefan Grimm <motionsystems@grimm.com>
2025-04-15 19:12:21 +0200

    Update nuget versions (#5)
    * Update nuget package versions.
    
    * Add book.db to solution.

--- books-app/csharp/GrpcBookInfoClient/Program.cs
+++ books-app/csharp/GrpcBookInfoClient/Program.cs
@@ -6,7 +6,9 @@ using System;
 
 var client = new BookInfoService.BookInfoServiceClient(GrpcChannel.ForAddress("https://localhost:5201"));
 
-var resp = await client.GetBookInfoWithReviewsAsync(new GetBookInfoRequest { Isbn = 12348 });
+// 12347 sometimes fails, sometime passes
+// var resp = await client.GetBookInfoWithReviewsAsync(new GetBookInfoRequest { Isbn = 12347 });
+var resp = await client.GetBookInfoWithReviewsAsync(new GetBookInfoRequest { Isbn = 12346 });
 
 Console.WriteLine($"Isbn:{resp.Isbn}, Name:{resp.Name}, Publisher:{resp.Publisher}");
 
