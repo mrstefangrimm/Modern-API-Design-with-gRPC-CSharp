d0970e60e468fd71450239d767c79468c819ffc4
mrstefangrimm <motionssystems@grimm.com>
2025-05-06 20:06:00 +0200

    Simplify.

--- loadbalancing/csharp/GreetClient/src/Program.cs
+++ loadbalancing/csharp/GreetClient/src/Program.cs
@@ -1,48 +1,30 @@
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Newtonsoft.Json;
 using Proto;
 using System;
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 
-var portString = Environment.GetEnvironmentVariable("PORT");
-int port = portString != null ? int.Parse(portString) : 8080;
-var host = Environment.GetEnvironmentVariable("HOST") ?? "0.0.0.0";
+var builder = WebApplication.CreateSlimBuilder(args);
 
-var grpcPortString = Environment.GetEnvironmentVariable("GATEWAY_PORT");
-int grpcPort = grpcPortString != null ? int.Parse(grpcPortString) : 50051;
-var grpcHost = Environment.GetEnvironmentVariable("GATEWAY_HOST") ?? "greet-server"; //minikube getting started:greet-server, kube: greetserver, kubectl get services; // when compose is used: "host.docker.internal";
+Console.WriteLine("Hello.");
 
-Console.WriteLine("Hello");
+var grpcAddress = builder.Configuration["GrpcGreetServerUrl"]!;
 
-//var channelCredentials = ChannelCredentials.Insecure;
-//var channel = new Channel($"{host}:{port}", channelCredentials);
+Console.WriteLine($"Expecting grpc greet server address: {grpcAddress}");
 
-string address = $"http://{grpcHost}:{grpcPort}";
-Console.WriteLine(address);
-using var channel = GrpcChannel.ForAddress(address);
+using var channel = GrpcChannel.ForAddress(grpcAddress);
 var client = new GreetService.GreetServiceClient(channel);
 
-var builder = WebApplication.CreateSlimBuilder(args);
-builder.WebHost.ConfigureKestrel(options =>
-{
-  options.Listen(IPAddress.Parse(host), port, listenOptions =>
-  {
-    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-  });
-});
 var app = builder.Build();
 
 var greetApi = app.MapGroup("/");
 
 greetApi.MapPost("/greet", async Task<string> (HttpRequest request) =>
 {
-  Console.WriteLine($"MapPost, grpc address:{address}");
+  Console.WriteLine($"MapPost, grpc address:{grpcAddress}");
 
   using var body = new StreamReader(request.Body);
   var data = await body.ReadToEndAsync();
