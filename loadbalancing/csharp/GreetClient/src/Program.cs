65f6bc438b982d3e1783294a41a35c14631dad0e
Stefan Grimm <motionsystems@grimm.com>
2025-08-27 17:24:14 +0200

    Add health check paths.

--- loadbalancing/csharp/GreetClient/src/Program.cs
+++ loadbalancing/csharp/GreetClient/src/Program.cs
@@ -23,7 +23,7 @@ var greetApi = app.MapGroup("/");
 
 greetApi.MapPost("/greet", async Task<string> (HttpRequest request) =>
 {
-  Console.WriteLine($"MapPost, grpc address:{grpcAddress}");
+  Console.WriteLine($"MapPost /greet, grpc address:{grpcAddress}.");
 
   using var body = new StreamReader(request.Body);
   var data = await body.ReadToEndAsync();
@@ -42,9 +42,34 @@ greetApi.MapPost("/greet", async Task<string> (HttpRequest request) =>
 
 greetApi.MapGet("/", () =>
 {
-  Console.WriteLine("MapGet");
+  Console.WriteLine($"MapGet /.");
 
-  return "Client got greet.";
+  string podIp = Environment.GetEnvironmentVariable("POD_IP") ?? string.Empty;
+  string podName = Environment.GetEnvironmentVariable("POD_NAME") ?? string.Empty;
+
+  return $"Client got greet with pod: name({podName}), ip({podIp}).";
 });
+
+greetApi.MapGet("/health-readiness", () =>
+{
+  Console.WriteLine($"MapGet /health-readiness.");
+
+  return "health-ready";
+});
+
+greetApi.MapGet("/health-liveness", () =>
+{
+  Console.WriteLine($"MapGet /health-liveness.");
+
+  return "health-alive";
+});
+
+greetApi.MapGet("/health-startup", () =>
+{
+  Console.WriteLine($"MapGet /health-startup.");
+
+  return "health-started";
+});
+
 
 app.Run();
